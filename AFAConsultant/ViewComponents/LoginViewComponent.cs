3367e7322e295f5f4229013e7c43526f3ff7a28e
Muhammad Naseer <115685117+MuhammadNaseer039@users.noreply.github.com>
2025-01-07 00:36:11 +0500

    ....

---
+++ AFAConsultant/ViewComponents/LoginViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using AFAConsultant.Data;
+using AFAConsultant.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AFAConsultant.ViewComponents
+{
+    public class LoginViewComponent : ViewComponent
+    {
+        private readonly AppdbContext db;
+
+        public Settings Settings { get; set; }
+
+        public LoginViewComponent(AppdbContext _db)
+        {
+            db = _db;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            Settings = await db.tbl_settings.FirstOrDefaultAsync();
+            return View(Settings);
+        }
+    }
+}
